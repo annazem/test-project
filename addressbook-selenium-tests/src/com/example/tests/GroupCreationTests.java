0fba7fac7c8af582e61514f1a227cdb07edcb987
annazem <anna_zem_iva@mail.ru>
2015-06-24 15:56:38 +0300

    1
    1

---
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -0,0 +1,29 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupCreationTests extends TestBase {
+  @Test
+  public void testNonEmptyGroupCreation() throws Exception {
+	openMaimPage();
+	gotoGroupsPage();
+    initGroupCreation();
+    GroupData group = new GroupData();
+    group.name = "group name 1";
+    group.header = "header1 1";
+    group.footer = "footer 1";
+	fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+
+  @Test
+  public void testEmptyGroupCreation() throws Exception {
+	openMaimPage();
+	gotoGroupsPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("", "", ""));
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+}
